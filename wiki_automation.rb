9e192a81825ccb48964584fe83c0fdd117de9dfe
Andy Fever <andy.fever@gmail.com>
2017-10-02 00:26:31 +0100

    Basic working solution for exercise

--- wiki_automation.rb
+++ wiki_automation.rb
@@ -2,12 +2,30 @@ require 'selenium-webdriver'
 require 'wrong'
 include Wrong
 
+=begin
+Setup Instructions
+------------------
+
+Ruby Version: 2.3.1
+Selenium Webdriver v 3.6.0
+'Wrong' Assertion Library 'https://github.com/sconover/wrong'
+
+=end
+
 def setup
   @driver = Selenium::WebDriver.for :chrome
 end
 
 def teardown
   @driver.quit
+end
+
+def setup_tests
+  @wikipedia = {url: 'http://wikipedia.org',
+                title: 'Wikipedia',
+                search_term: 'fish',
+                text: 'Fish are the gill-bearing aquatic craniate animals that lack limbs with digits',
+                language: 'English'}
 end
 
 def run
@@ -32,6 +50,10 @@ run do
   end
 
   @driver.find_element(:xpath, '//*[@id="search-form"]/fieldset/button').click
+
+  # Check the fist heading matches the search string
   assert {@driver.find_element(:css, '#firstHeading').text == @wikipedia[:search_term] || @wikipedia[:search_term].capitalize}
+
+  # Check the page is displayed in English
   assert {(@driver.find_element(:css, '#mw-content-text').text.include? @wikipedia[:text]) ==  true}
 end
