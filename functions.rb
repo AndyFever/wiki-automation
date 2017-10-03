9a1543826acda0a1d32e4131b4fa6f49f68cabd4
Andy Fever <andy.fever@gmail.com>
2017-10-03 17:48:38 +0100

    Clean up of code logic

--- functions.rb
+++ functions.rb
@@ -1,10 +1,5 @@
 # This contains global functions that can be used in other tests
 
 def select_dropdown(options, language)
-  options.each do |option|
-    if option.text == language
-      option.click
-      break
-    end
-  end
+  options.each { |option| option.click if option.text == language }
 end
