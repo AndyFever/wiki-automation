bc52604d152d491a06505486c2f12f1a01aa187d
Andy Fever <andy.fever@gmail.com>
2017-10-03 17:26:14 +0100

    Added test case in with a german search

--- wiki_automation.rb
+++ wiki_automation.rb
@@ -1,7 +1,7 @@
 require 'selenium-webdriver'
 require 'wrong'
 include Wrong
-
+require 'pry'
 =begin
 
 Setup Requirements
@@ -23,11 +23,20 @@ def teardown
 end
 
 def setup_tests
-  @wikipedia = {url: 'http://wikipedia.org',
-                title: 'Wikipedia',
-                search_term: 'fish',
-                text: 'Fish are the gill-bearing aquatic craniate animals that lack limbs with digits',
-                language: 'English'}
+  wikipedia_english = {url: 'http://wikipedia.org',
+                        title: 'Wikipedia',
+                        search_term: 'fish',
+                        text: 'Fish are the gill-bearing aquatic craniate animals that lack limbs with digits',
+                        language: 'English'}
+  wikipedia_dutch = {url: 'http://wikipedia.org',
+                        title: 'Wikipedia',
+                        search_term: 'Frankreich',
+                        text: 'Die Fläche der gesamten Französischen Republik',
+                        language: 'Deutsch'}
+
+@all_tests = []
+@all_tests.push(wikipedia_english)
+@all_tests.push(wikipedia_dutch)
 end
 
 def run
@@ -38,24 +47,26 @@ def run
 end
 
 run do
-  @driver.get @wikipedia[:url]
-  assert {@driver.title == @wikipedia[:title]}
-  searchBox = @driver.find_element(:css, '#searchInput')
-  searchBox.send_keys(@wikipedia[:search_term])
-  select = @driver.find_element(:css, '#searchLanguage')
-  drop_down = select.find_elements(:tag_name, "option")
-  drop_down.each do |option|
-    if option.text == @wikipedia[:language]
-      option.click
-      break
+  @all_tests.each do |test|
+    @driver.get test[:url]
+    assert {@driver.title == test[:title]}
+    searchBox = @driver.find_element(:css, '#searchInput')
+    searchBox.send_keys(test[:search_term])
+    select = @driver.find_element(:css, '#searchLanguage')
+    drop_down = select.find_elements(:tag_name, "option")
+    drop_down.each do |option|
+      if option.text == test[:language]
+        option.click
+        break
+      end
     end
+
+    @driver.find_element(:xpath, '//*[@id="search-form"]/fieldset/button').click
+
+    # Check the fist heading matches the search string
+    assert {@driver.find_element(:css, '#firstHeading').text == test[:search_term] || test[:search_term].capitalize}
+
+    # Check the page is displayed in English
+    assert {(@driver.find_element(:css, '#mw-content-text').text.include? test[:text]) ==  true}
   end
-
-  @driver.find_element(:xpath, '//*[@id="search-form"]/fieldset/button').click
-
-  # Check the fist heading matches the search string
-  assert {@driver.find_element(:css, '#firstHeading').text == @wikipedia[:search_term] || @wikipedia[:search_term].capitalize}
-
-  # Check the page is displayed in English
-  assert {(@driver.find_element(:css, '#mw-content-text').text.include? @wikipedia[:text]) ==  true}
 end
